fffb77bdfebd94e9a346c482db47eeb92b213494
malmiteria <malmiteria@gmail.com>
2023-10-07 15:41:01 +0200

    added the rest framework API over the pokemon model

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from pokedex.viewsets import PokemonViewSet
+
+router = routers.DefaultRouter()
+router.register("pokemon", PokemonViewSet, basename="pokemon")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("pokedex/", include(router.urls)),
 ]
---
+++ pokedex/viewsets.py
@@ -0,0 +1,10 @@
+from rest_framework import filters, viewsets
+from pokedex.models import Pokemon
+from pokedex.serializers import PokemonSerializer
+
+
+class PokemonViewSet(viewsets.ModelViewSet):
+    queryset = Pokemon.objects.all()
+    serializer_class = PokemonSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name"]
